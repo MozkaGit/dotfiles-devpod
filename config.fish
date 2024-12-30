a7d3322baaf85c8b3d0275ffadb4964e3b5e73db
MozkaGit <mozkaops@gmail.com>
2024-12-30 04:51:56 +0100

    add fish configuration and customization with starship

---
+++ config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Initialize starship prompt for a customized shell appearance
+/home/linuxbrew/.linuxbrew/bin/starship init fish | source
+
+# Enable transience for cleaner command line history
+enable_transience
+
+# Initialize asdf version manager for managing runtime versions
+source /home/linuxbrew/.linuxbrew/bin/asdf
+
+# Set the default system editor to nvim
+set -gx EDITOR nvim
+
+# Add brew to PATH
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
